b62433b014247e7c482b6ca65a85a9c54516c1aa
김승균_나인트리 <holosum@ninetree.com>
2022-12-22 17:48:37 +0900

    기본실행코드 완료

--- vue-graphql/vue.config.js
+++ vue-graphql/vue.config.js
@@ -1,4 +1,62 @@
+'use strict';
+const path = require('path')
 const { defineConfig } = require('@vue/cli-service')
+// https://webpack.js.org/configuration/dev-server/#devserver-proxy
+
+const targetHost = 'localhost';
+const targetPort = 8080;
+
+const port = 3000;
+// let cookie;
+//
+// const replaceCookie = function(cookie) {
+//     return cookie
+//         .replace(/\s+domain=[^\s;]+;?/, '')
+//         .replace(/\s+secure;?/, '')
+// };
+
 module.exports = defineConfig({
-  transpileDependencies: true
-})
+  css: {
+    loaderOptions: {
+      scss: {
+        additionalData: `
+          @import "~@/assets/scss/style.scss";
+        `
+      }
+    }
+  },
+  outputDir: path.resolve(__dirname, '../src/main/resources/static/'),
+  devServer: {
+    port: port,
+    // clientLogLevel: 'debug',
+    // historyApiFallback: true,
+    // hot: true,
+    // inline: true,
+    headers: {
+      'Access-Control-Allow-Origin': '*'
+    },
+    proxy: {
+      '/api' : {
+        target: `http://${targetHost}:${targetPort}`, //개발서버
+        changeOrigin: true,
+        logLevel: 'debug'
+      },
+      '/ws' : {
+        target: `http://${targetHost}:${targetPort}`, //개발서버
+        changeOrigin: true,
+        ws: true,
+        logLevel: 'debug'
+      }
+    }
+  },
+
+  pluginOptions: {
+    // moment: {
+    //   locales: ['ko_kr']
+    // },
+    quasar: {
+      importStrategy: 'kebab',
+      rtlSupport: false
+    }
+  }
+});
